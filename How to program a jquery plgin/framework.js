c7fb400884f9b50d0acb0a2acdfe364632724b6c
yuhualingfeng <1135317965@qq.com>
2015-08-13 10:07:39 +0800

    how to program a jqery plug-in

---
+++ How to program a jquery plgin/framework.js
@@ -0,0 +1,16 @@
+
+/**
+ the module of insert your plug-in code
+ copyright 2015,puhuasheng
+ */
+ ;(function($,window){
+	 
+	 //you can code here prepare for you plug-in
+	 
+	 $.fn.methodName=function(options){
+		 
+		 // code what you want
+	 };
+	 
+ })(JQuery,window);
+
